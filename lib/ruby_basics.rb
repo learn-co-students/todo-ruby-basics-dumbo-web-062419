357a796a51fb0d6aeac6fc29aa6143b6d0eae446
JahazielGuzman <jahaziel.guzman93@gmail.com>
2019-06-06 15:51:57 -0400

    got all tests to pass

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+	num1 / num2	
 end
 
-def assign_variable(value)
-  
+def assign_variable(named)
+	named
 end
 
-def argue
-  
+def argue(arrr)
+  arrr
 end
 
-def greeting
-
+def greeting(greeting, name)
+	"#{greeting} #{name} how are you doing today?"
 end
 
 def return_a_value
-
+	"Nice"
 end
 
 def last_evaluated_value
-
+	2 + 2
+	3 * 3
+	"expert"
 end
 
-def pizza_party
-
+def pizza_party(topping="cheese")
+	topping
 end
